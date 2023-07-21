626a8e3cfec20737655ab8205c4cd13031c59532
robertej19 <robertej@mit.edu>
2023-07-21 09:56:38 -0400

    production run of GK model

---
+++ T_GK_model/july_2023_running/runlist_2.sh
@@ -0,0 +1,3 @@
+#./Pi0_GK_Vegas_July_2023 0.125 0.275 .05 1.25 8.75 0.5 test_july_final
+./Pi0_GK_Vegas_July_2023 0.325 0.475 .05 1.25 8.75 0.5 test_july_final
+#./Pi0_GK_Vegas_July_2023 0.525 0.675 .05 1.25 8.75 0.5 test_july_final
